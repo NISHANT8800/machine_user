89bbd002f46b13471cb83f8e6220a4e821b855c9
mahajannishant007@gmail.com <mahajannishant007@gmail.com>
2024-08-21 20:11:19 +0530

    first comit

---
+++ src/main/java/com/user/users_management/repository/UsersRepo.java
@@ -0,0 +1,11 @@
+package com.user.users_management.repository;
+
+import com.user.users_management.entity.OurUsers;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsersRepo extends JpaRepository<OurUsers, Integer> {
+
+    Optional<OurUsers> findByEmail(String email);
+}
